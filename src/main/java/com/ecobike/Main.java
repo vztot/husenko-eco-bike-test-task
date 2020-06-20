163b94382c4210ff3184649a0bc1d842210947a5
Oleksandr Husenko <oleksandrhusenko@gmail.com>
2020-06-20 17:27:25 +0300

    Finished App context

--- src/main/java/com/ecobike/Main.java
+++ src/main/java/com/ecobike/Main.java
@@ -1,26 +1,23 @@
 package com.ecobike;
 
 import com.ecobike.context.ApplicationContext;
-import com.ecobike.model.Bike;
-import com.ecobike.model.entity.FoldingBike;
 import com.ecobike.service.BikeService;
-import com.ecobike.util.tree.BinaryTree;
-import com.ecobike.util.tree.impl.MyBinaryTreeImpl;
-import java.math.BigDecimal;
 import org.apache.log4j.Logger;
 
 public class Main {
     private static final Logger LOGGER = Logger.getLogger(Main.class);
-    private static final ApplicationContext ctx = new ApplicationContext();
     private static final BikeService bikeService =
-            (BikeService) ctx.getServiceByName("BikeService");
+            (BikeService) ApplicationContext.getInstance().getService(BikeService.class);
 
     public static void main(String[] args) {
-        BinaryTree<Bike> tree = new MyBinaryTreeImpl<>();
-        FoldingBike bike = new FoldingBike("HUY", 100, true, "black",
-                new BigDecimal(100), 10, 10);
-        tree.add(bike);
-        tree.add(bike);
-        tree.add(bike);
+//        BinaryTree<Bike> tree = new MyBinaryTreeImpl<>();
+//        FoldingBike bike = new FoldingBike("HUY", 100, true, "black",
+//                new BigDecimal(100), 10, 10);
+//        tree.add(bike);
+//        tree.add(bike);
+//        tree.add(bike);
+        Controller controller = new Controller();
+        System.out.println(bikeService);
+        System.out.println(controller.getServiceRef());
     }
 }
--- src/main/java/com/ecobike/context/ApplicationContext.java
+++ src/main/java/com/ecobike/context/ApplicationContext.java
@@ -1,30 +1,40 @@
 package com.ecobike.context;
 
-import com.ecobike.service.BikeService;
 import com.ecobike.util.Injector;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ApplicationContext {
 
-    // This is a simple implementation of a context provider made like command pattern
+    // custom Application context aka singleton
 
-    private static final Map<String, Object> SERVICES;
-    private static final Injector INJECTOR = Injector.getInstance("com.ecobike");
-    private static BikeService bikeService = (BikeService) INJECTOR.getInstance(BikeService.class);
+    private static ApplicationContext ctx;
 
-    static {
-        final Map<String, Object> services = new HashMap<>();
-        services.put("BikeService", bikeService);
-        SERVICES = Collections.unmodifiableMap(services);
+    private Injector injector;
+    private Map<String, Object> services;
+
+    private ApplicationContext() {
+        injector = Injector.getInstance("com.ecobike");
+        this.services = new HashMap<>();
     }
 
-    public Object getServiceByName(String serviceName) {
-        Object service = SERVICES.get(serviceName);
+    public Object getService(Class clazz) {
+        Object service = services.get(clazz.getName());
         if (service == null) {
-            throw new RuntimeException("Cant get service: " + serviceName);
+            try {
+                service = injector.getInstance(clazz);
+                services.put(clazz.getName(), service);
+            } catch (Exception e) {
+                throw new RuntimeException("Cant get service: " + clazz.getName());
+            }
         }
         return service;
+    }
+
+    public static ApplicationContext getInstance() {
+        if (ctx == null) {
+            ctx = new ApplicationContext();
+        }
+        return ctx;
     }
 }
